9b5149ff9ae31df0133367cfde92db09c03fe64b
Lidor Elmakaies <lidoralmkaysworkmail@gmail.com>
2024-12-23 19:48:38 +0200

    Add validation on DTO's

--- Domain/DTOs/GenerateTokenInfo.dto.cs
+++ Domain/DTOs/GenerateTokenInfo.dto.cs
@@ -1,13 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain.DTOs
 {
     public class GenerateTokenInfoDTO
     {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address format.")]
         public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
         public required string Password { get; set; }
     }
 }
--- Domain/DTOs/UserIdentifyingInformation.dto.cs
+++ Domain/DTOs/UserIdentifyingInformation.dto.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Domain.DTOs
 {
     public class UserIdentifyingInformationDTO
     {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address format.")]
         public required string Email { get; set; }
     }
 }
